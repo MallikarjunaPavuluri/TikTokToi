e3ddcf1749fcebcbbcc73a0eb0f6a84c16be97c6
MallikarjunaPavuluri <74953576+MallikarjunaPavuluri@users.noreply.github.com>
2020-12-22 15:51:56 +0530

    TikTokToi
    It is Console Based Duo game. Each player place their mark in 3X3 box one after other elected Box If there are any match in horizontal or vertical or diagonal of same mark that mark player is winner, In this system generated toss winner mark is 'X' and other one assigned 'O'.

---
+++ GuestPlayer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTokToi
+{
+    class GuestPlayer 
+    {
+        public GuestPlayer(string name)
+        {
+            Name = name;
+        }
+
+        public String Name
+        {
+            get;
+        }
+        
+        private static GuestPlayer Guest()
+        {
+            Console.WriteLine("Enter Your Name -");
+            var name = Console.ReadLine();
+            GuestPlayer p = new GuestPlayer(name);
+            return p;
+        }
+
+        public static  void GameStart()
+        {
+            int box = 0;
+            
+            GuestPlayer P1 = Guest();
+
+            GuestPlayer P2 = Guest();
+            GuestPlayer Swap;
+            Random coin = new Random();
+            int toss = coin.Next(0, 2); 
+            if(toss==1)
+            {
+                Console.WriteLine($"System Toss Winner is {P2.Name}");
+                Swap = P1;
+                P1 = P2;
+                P2 = Swap;
+                
+            }
+            else
+            {
+                Console.WriteLine($"System Toss Winner is {P1.Name}");
+            }
+            
+            Game.Display();
+            Game.Reset();
+            var index = " ";
+
+            for (int i = 0; i < 9; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{P1.Name} Enter Box No");
+                    index = Console.ReadLine();
+                    box = Game.CheckIndex(index);
+                    Game.Add(box, 'X');
+
+                }
+                else
+                {
+                    Console.WriteLine($"{P2.Name} Enter Box No");
+                    index = Console.ReadLine();
+                    box = Game.CheckIndex(index);
+                    Game.Add(box, 'O');
+
+                }
+                     if (i >= 4 && Game.WinnerCheck())
+                     {
+                         if (i % 2 == 0)
+                         {
+                             Console.WriteLine($"Winner Is {P1.Name}");
+                             break;
+                         }
+                         else
+                         {
+                             Console.WriteLine($"Winner Is {P2.Name}");
+                             break;
+                         }
+                     }
+                Game.Display();
+
+            }
+            if ((!Game.WinnerCheck()))
+            {
+                Console.WriteLine("Oh Match Draw");
+            }
+            Game.Display();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TikTokToi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\tHey Welcome To InnRoad TIK TOk TOI Game:");
+            Console.WriteLine("\t\t\t________________________________________\n\n");
+            
+            // Console.WriteLine("Select One 1.Login Or Create Account\n\t  2.Login As Guest ");
+            Console.WriteLine("Currently We Are Working On DataBase Continue  Login As Guest");
+            /* var index = Console.ReadLine();
+             int n = 0;
+             while (!(int.TryParse(index, out n)))
+             {
+                 Console.WriteLine("\t\t\tInvalid Selection Enter Again");
+                 index = Console.ReadLine();
+             }
+             if (n == 2)
+             {
+
+                 GuestPlayer.GameStart();
+             }
+             else
+             {
+                 PlayersDetails.GameStart();
+             } */
+             GuestPlayer.GameStart();
+
+
+        }
+    }
+}
